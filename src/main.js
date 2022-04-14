ab017ef148c71fc2c5cb182444c311dc1820e495
Dipen Parmar <dipensavji@gmail.com>
2022-04-14 19:33:21 +0530

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import router from "./router";
 
 const app = createApp(App);
+
+// Make sure to _use_ the router instance to make the
+// whole app router-aware.
+app.use(router);
 
 app.mount("#app");
